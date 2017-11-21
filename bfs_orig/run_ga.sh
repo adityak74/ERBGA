42f04146eefe9b54d82ce8b6c74f9bd30d94b1c9
Aditya <akarnam37@gmail.com>
2017-11-20 23:05:41 -0600

    Added comment ability to datasets.txt

--- bfs_orig/run_ga.sh
+++ bfs_orig/run_ga.sh
@@ -2,7 +2,11 @@
 if [ $1 ]
 then
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    sbatch run_sbatch_ga.sh $line
+    # sbatch run_sbatch_ga.sh $line
+    if [ ${line:0:1} != "#" ]
+    then 
+        sbatch run_sbatch_ga.sh $line
+    fi
 done < "$1"
 else
 echo "Usage: ./run_ga.sh datasets.txt"
