5d5875870b03ac528210cfff77820d60e49b082a
Aditya <akarnam37@gmail.com>
2018-03-09 20:01:27 -0600

    Move make to start

--- bfs_orig/run_ga.sh
+++ bfs_orig/run_ga.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+make
 if [ $1 ]
 then
 while IFS='' read -r line || [[ -n "$line" ]]; do
     # sbatch run_sbatch_ga.sh $line
     if [ ${line:0:1} != "#" ]
     then
-	make #run make for compiling bfs
 	for i in {1..25}
 	do
         	sbatch run_sbatch_ga.sh $line
